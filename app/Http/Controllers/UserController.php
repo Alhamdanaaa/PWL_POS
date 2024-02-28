7cc75d1e6a1da0666686ddfb105957b91569f27e
Alhamdanaaa <aalhamdana@gmail.com>
2024-02-28 20:48:25 +0700

    Praktikum-6

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index(){
+    // Prak 6(awal)
+        // // tambah data user dengan Eloquent Model
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan', 
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        // ];
+        // UserModel::insert($data);//tambah data ke tabel
+
+        // // coba akses model UserModel
+        // $user = UserModel::all();//ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+
+    //Prak 6 (Akhir) 
+        // tambah user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data);//update data user
+        
+        // coba akses model UserModel
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+}
