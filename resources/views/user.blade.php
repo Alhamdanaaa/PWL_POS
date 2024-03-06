685a693b3a358b7326d079c961262dd8796b227f
Alhamdanaaa <aalhamdana@gmail.com>
2024-03-06 09:38:02 +0700

    Prak-2.3 Jobsheet 4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,6 +7,12 @@
     <h1>Data User</h1>
     <table border="1" cellpading="2" cellspacing="0">
         <tr>
+            <th>Jumlah Pengguna</th>
+        </tr>
+        <tr>
+            <td>{{ $data }}</td>
+        </tr>
+        {{-- <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
@@ -17,7 +23,7 @@
             <td>{{ $data->username }}</td>
             <td>{{ $data->nama }}</td>
             <td>{{ $data->level_id }}</td>
-        </tr>
+        </tr> --}}
 
     </table>
 </body>
