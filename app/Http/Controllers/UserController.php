cd1cfb011bf96a045ff3ab7cbedefaadadbbd29e
Alhamdanaaa <aalhamdana@gmail.com>
2024-03-06 09:29:15 +0700

    Prak-2.2 Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -47,9 +47,13 @@ class UserController extends Controller
         // return view('user', ['data' => $user]);
         // $user = UserModel::firstwhere('level_id',1);
         // return view('user', ['data' => $user]);
-        $user = UserModel::findor(20,['username','nama'],function(){
-            abort(404);
-        });
+        // $user = UserModel::findor(20,['username','nama'],function(){
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+        // $user = UserModel::findOrFail(1);
+        // return view('user', ['data' => $user]);
+        $user = UserModel::where('username','manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
